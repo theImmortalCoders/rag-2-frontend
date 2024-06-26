69cee32764973d300665f120cac72809474c74ff
Paweł Buczek <pbuczek7@wp.pl>
2024-06-26 18:37:32 +0200

    refactor: add prettier and changed files structure

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
 
 module.exports = tseslint.config(
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
@@ -14,120 +14,120 @@ module.exports = tseslint.config(
     ],
     languageOptions: {
       parserOptions: {
-        project: "tsconfig.json",
+        project: 'tsconfig.json',
       },
     },
     processor: angular.processInlineTemplates,
     rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
+      '@angular-eslint/directive-selector': [
+        'error',
         {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
         },
       ],
-      "@angular-eslint/component-selector": [
-        "error",
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
         },
       ],
-      semi: "off",
-      "@typescript-eslint/semi": "off",
-      "@typescript-eslint/consistent-type-imports": "off",
+      semi: 'off',
+      '@typescript-eslint/semi': 'off',
+      '@typescript-eslint/consistent-type-imports': 'off',
       // Note: you must disable the base rule as it can report incorrect errors
-      "space-before-function-paren": "off",
-      "@typescript-eslint/space-before-function-paren": "off",
-      "@typescript-eslint/member-delimiter-style": "off",
-      "comma-dangle": "off",
-      "@typescript-eslint/comma-dangle": "off",
-      "@typescript-eslint/no-misused-promises": [
-        "error",
+      'space-before-function-paren': 'off',
+      '@typescript-eslint/space-before-function-paren': 'off',
+      '@typescript-eslint/member-delimiter-style': 'off',
+      'comma-dangle': 'off',
+      '@typescript-eslint/comma-dangle': 'off',
+      '@typescript-eslint/no-misused-promises': [
+        'error',
         {
           checksVoidReturn: false,
         },
       ],
-      "@typescript-eslint/no-non-null-assertion": "warn",
-      "no-use-before-define": ["error", { variables: true }],
-      "@typescript-eslint/no-extraneous-class": "warn",
-      "@typescript-eslint/naming-convention": [
-        "warn",
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      'no-use-before-define': ['error', { variables: true }],
+      '@typescript-eslint/no-extraneous-class': 'warn',
+      '@typescript-eslint/naming-convention': [
+        'warn',
         {
-          selector: "class",
-          format: ["PascalCase"],
+          selector: 'class',
+          format: ['PascalCase'],
         },
         {
-          selector: "typeLike",
-          format: ["PascalCase"],
+          selector: 'typeLike',
+          format: ['PascalCase'],
         },
         {
-          selector: ["enum", "enumMember"],
-          format: ["PascalCase"],
+          selector: ['enum', 'enumMember'],
+          format: ['PascalCase'],
         },
         {
-          selector: ["memberLike", "variableLike"],
-          modifiers: ["const", "exported", "readonly"],
-          format: ["PascalCase"],
+          selector: ['memberLike', 'variableLike'],
+          modifiers: ['const', 'exported', 'readonly'],
+          format: ['PascalCase'],
         },
         {
-          selector: ["parameter", "method", "function"],
-          format: ["camelCase"],
-          leadingUnderscore: "allow",
+          selector: ['parameter', 'method', 'function'],
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
         },
         {
-          selector: "interface",
-          format: ["PascalCase"],
+          selector: 'interface',
+          format: ['PascalCase'],
           custom: {
-            regex: "^I[A-Z]",
+            regex: '^I[A-Z]',
             match: true,
           },
         },
         {
-          selector: "typeAlias",
-          format: ["PascalCase"],
+          selector: 'typeAlias',
+          format: ['PascalCase'],
           custom: {
-            regex: "^T[A-Z]",
+            regex: '^T[A-Z]',
             match: true,
           },
         },
         {
-          selector: ["variable", "classProperty"],
-          types: ["boolean"],
-          format: ["PascalCase"],
+          selector: ['variable', 'classProperty'],
+          types: ['boolean'],
+          format: ['PascalCase'],
           prefix: [
-            "can",
-            "did",
-            "has",
-            "is",
-            "must",
-            "needs",
-            "should",
-            "will",
+            'can',
+            'did',
+            'has',
+            'is',
+            'must',
+            'needs',
+            'should',
+            'will',
           ],
         },
         {
-          selector: ["variable", "classProperty"],
-          modifiers: ["public"],
-          format: ["camelCase"],
-          leadingUnderscore: "forbid",
+          selector: ['variable', 'classProperty'],
+          modifiers: ['public'],
+          format: ['camelCase'],
+          leadingUnderscore: 'forbid',
         },
         {
-          selector: "memberLike",
-          modifiers: ["private"],
-          format: ["camelCase"],
-          leadingUnderscore: "require",
+          selector: 'memberLike',
+          modifiers: ['private'],
+          format: ['camelCase'],
+          leadingUnderscore: 'require',
         },
       ],
-      "max-depth": ["warn", 1],
-      complexity: ["warn", { max: 5 }],
-      "max-lines": ["warn", 200],
-      "no-else-return": "warn",
-      "no-console": "off",
-      "no-magic-numbers": [
-        "warn",
+      'max-depth': ['warn', 1],
+      complexity: ['warn', { max: 5 }],
+      'max-lines': ['warn', 200],
+      'no-else-return': 'warn',
+      'no-console': 'off',
+      'no-magic-numbers': [
+        'warn',
         {
           detectObjects: false,
           enforceConst: true,
@@ -135,26 +135,26 @@ module.exports = tseslint.config(
           ignoreArrayIndexes: true,
         },
       ],
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
         {
-          args: "all",
-          argsIgnorePattern: "^_",
-          caughtErrors: "none",
-          caughtErrorsIgnorePattern: "^_",
-          vars: "all",
-          varsIgnorePattern: "^[_A-Z]",
-          destructuredArrayIgnorePattern: "^_",
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'none',
+          caughtErrorsIgnorePattern: '^_',
+          vars: 'all',
+          varsIgnorePattern: '^[_A-Z]',
+          destructuredArrayIgnorePattern: '^_',
           ignoreRestSiblings: false,
         },
       ],
-      "no-unused-vars": "off",
-      "@typescript-eslint/explicit-function-return-type": "error",
-      "@typescript-eslint/explicit-member-accessibility": "error",
+      'no-unused-vars': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-member-accessibility': 'error',
     },
   },
   {
-    files: ["**/*.html"],
+    files: ['**/*.html'],
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  template: '',
+  styles: '',
 })
 export class AppComponent {
   private _title = 'rag-2-frontend';
-  dupa = false;
 }
