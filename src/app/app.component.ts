ac6a51ed56b6edf6821c66b2c5e79a39dec2a6ea
Paweł Buczek <pbuczek7@wp.pl>
2024-06-27 02:10:51 +0200

    feat: tailwind tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,20 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: '<div class="text-red-600">{{title}}</div> ',
+  template: `<nav
+    class="bg-mainGray min-w-max pl-6 py-4 shadow-navbarShadow relative z-50">
+    <div class="flex mx-auto w-full flex-row items-center justify-between">
+      <div
+        class="flex w-1/2 xs:w-[60%] sm:w-5/12 lg:w-1/3 justify-between items-center">
+        <a href="/">
+          <img src="" alt="Logo" class="h-12 w-auto" />
+        </a>
+        <span class="text-3xl font-mono text-mainOrange hidden xs:block"
+          >RUT-AI GAMES</span
+        >
+      </div>
+    </div>
+  </nav>`,
 })
 export class AppComponent {
   public title = 'rag-2-frontend';
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,60 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '2xs': '375px',
+        xs: '475px',
+        mds: '799px',
+        mdl: '910px',
+        '3xl': '1760px',
+      },
+      colors: {
+        mainGray: '#454545',
+        lightGray: '#5D5D5D',
+        mainOrange: '#FF6000',
+        lightOragne: '#FFA559',
+        mainCreme: '#FFE6C7',
+      },
+      height: {
+        navbar: '80px',
+      },
+      padding: {
+        navbar: '80px',
+      },
+      minHeight: {
+        all: 'calc(100vh - 80px)',
+      },
+      fontSize: {
+        '2xs': '0.625rem',
+        '3xs': '0.5rem',
+      },
+      lineHeight: {
+        '2xs': '0.8125rem',
+        '3xs': '0.625rem',
+      },
+      boxShadow: {
+        navbarShadow: '0px 1px 7px 2px rgba(255, 96, 0, 1)',
+        footerShadow: 'inset 0px 10px 23px 0px rgba(69, 69, 69, 1)',
+      },
+      animation: {
+        typewriter21: 'typewriter 3s steps(21) forwards',
+        caret21:
+          'typewriter 3s steps(21) forwards, blink 2s steps(21) infinite 3s',
+      },
+      keyframes: {
+        typewriter: {
+          to: { left: '100%' },
+        },
+        blink: {
+          '0%': { opacity: '0' },
+          '0.1%': { opacity: '1' },
+          '50%': { opacity: '1' },
+          '50.1%': { opacity: '0' },
+          '100%': { opacity: '0' },
+        },
+      },
+    },
   },
   plugins: [],
 };
