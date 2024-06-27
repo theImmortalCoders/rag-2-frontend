279f5c00bdab009ae89f28b08476717944e5d41e
Paweł Buczek <pbuczek7@wp.pl>
2024-06-27 12:10:43 +0200

    refactor: main app changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,9 @@ import { FooterComponent } from './shared/components/footer/footer.component';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
-  template: ` <app-navbar></app-navbar>
-    <main><router-outlet></router-outlet></main>
-    <app-footer></app-footer>`,
+  template: ` <app-navbar />
+    <main><router-outlet /></main>
+    <app-footer />`,
 })
 export class AppComponent {
   public title = 'rag-2-frontend';
