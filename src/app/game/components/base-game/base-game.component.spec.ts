a5a0df93239e4852ac3a6fba38c7b825bbfb1eb7
Paweł Buczek <pbuczek7@wp.pl>
2024-06-28 16:37:21 +0200

    feat: #7 added base-game components tests

--- src/app/game/components/base-game/base-game.component.spec.ts
+++ src/app/game/components/base-game/base-game.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BaseGameComponent } from './base-game.component';
+import { TMenuType } from '../../models/menu-type.enum';
+import { TimeMenuComponent } from '../menu/time-menu.component';
+import { EventMenuComponent } from '../menu/event-menu.component';
+import { NgComponentOutlet } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({ selector: 'app-dummy', template: '' })
+class DummyComponent {}
 
 describe('BaseGameComponent', () => {
   let component: BaseGameComponent;
@@ -8,15 +15,45 @@ describe('BaseGameComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BaseGameComponent],
+      imports: [BaseGameComponent, NgComponentOutlet],
+      declarations: [DummyComponent],
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(BaseGameComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set component based on menuType', () => {
+    component.menuType = 'TIME';
+    component.ngOnInit();
+    expect(component.component).toEqual(TimeMenuComponent);
+  });
+
+  it('should set null component when menuType is not recognized', () => {
+    component.menuType = 'INVALID_TYPE' as TMenuType; // Type assertion for invalid type
+    component.ngOnInit();
+    expect(component.component).toBeNull();
+  });
+
+  it('should render TimeMenuComponent when menuType is TIME', () => {
+    component.menuType = 'TIME';
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('app-time-menu')).not.toBeNull();
+  });
+
+  it('should render EventMenuComponent when menuType is EVENT', () => {
+    component.menuType = 'EVENT';
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('app-event-menu')).not.toBeNull();
+  });
+
+  // Add more tests as needed to cover other scenarios
 });
