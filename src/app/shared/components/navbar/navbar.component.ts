0104d0cb4bfed65e8fa4f47c4a417b2f4196cc9e
Paweł Buczek <pbuczek7@wp.pl>
2024-07-03 23:09:02 +0200

    feat: #11 created login page layout with reactive forms implemented

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,25 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import * as feather from 'feather-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   template: `
     <nav
       class="bg-darkGray min-w-max pl-6 py-4 shadow-navbarShadow relative z-50">
       <div class="flex mx-auto w-full flex-row items-center justify-between">
         <div
           class="flex w-1/2 xs:w-[60%] sm:w-5/12 lg:w-1/3 justify-between items-center">
-          <a href="/">
+          <a [routerLink]="['/']">
             <img src="" alt="Logo" class="h-12 w-auto" />
           </a>
           <span class="text-3xl font-mono text-mainOrange hidden xs:block"
             >RAG-2</span
           >
         </div>
+        <a [routerLink]="['/login']" class="mr-10"
+          ><i data-feather="users" class="text-mainOrange size-9"></i
+        ></a>
       </div>
     </nav>
   `,
   styles: ``,
 })
-export class NavbarComponent {}
+export class NavbarComponent implements AfterViewInit {
+  public ngAfterViewInit(): void {
+    feather.replace();
+  }
+}
