cc7f20657c328e2c039a142c96f4f84f50d858d8
Paweł Buczek <pbuczek7@wp.pl>
2024-07-05 00:51:06 +0200

    fix: #11 deleted unused code

--- src/app/user-workflow/login/login.page.component.ts
+++ src/app/user-workflow/login/login.page.component.ts
@@ -1,6 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import * as feather from 'feather-icons';
+import { Component } from '@angular/core';
 import { LoginFormComponent } from './components/login-form.component';
 import { BenefitsListComponent } from '../shared/components/benefits-list/benefits-list.component';
 import { SideFormPanelComponent } from '../shared/components/side-form-panel/side-form-panel.component';
@@ -8,6 +6,7 @@ import { SideFormPanelComponent } from '../shared/components/side-form-panel/sid
 @Component({
   selector: 'app-login-page',
   standalone: true,
+  imports: [LoginFormComponent, BenefitsListComponent, SideFormPanelComponent],
   template: `
     <div
       class="w-full min-h-screen bg-mainGray flex justify-evenly font-mono pt-20 text-mainCreme">
@@ -22,27 +21,7 @@ import { SideFormPanelComponent } from '../shared/components/side-form-panel/sid
       </div>
     </div>
   `,
-  imports: [
-    ReactiveFormsModule,
-    LoginFormComponent,
-    BenefitsListComponent,
-    SideFormPanelComponent,
-  ],
 })
-export class LoginPageComponent implements AfterViewInit {
-  public loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-  });
-
+export class LoginPageComponent {
   public mainTextValue = "Don't you have an account yet?";
-
-  public ngAfterViewInit(): void {
-    feather.replace();
-  }
-
-  public submitButton(): void {
-    console.log('Email: ', this.loginForm.value.email);
-    console.log('Password: ', this.loginForm.value.password);
-  }
 }
