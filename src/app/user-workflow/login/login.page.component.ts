a34432715d337946ce24d16de88fc0142cd5ebbb
Paweł Buczek <pbuczek7@wp.pl>
2024-07-04 19:34:11 +0200

    refactor: #11 divide into shared components

--- src/app/user-workflow/login/login.page.component.ts
+++ src/app/user-workflow/login/login.page.component.ts
@@ -1,59 +1,41 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import * as feather from 'feather-icons';
 import { LoginFormComponent } from './components/login-form.component';
+import { BenefitsListComponent } from '../shared/components/benefits-list/benefits-list.component';
+import { SideFormPanelComponent } from '../shared/components/side-form-panel/side-form-panel.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterModule, LoginFormComponent],
   template: `
     <div
       class="w-full min-h-screen bg-mainGray flex justify-evenly font-mono pt-20 text-mainCreme">
       <app-login-form
         class="w-1/3 h-fit border-2 border-mainOrange rounded-lg px-6 py-8" />
       <div class="flex flex-col space-y-4 w-1/3 h-fit p-6">
-        <h1 class="text-2xl font-bold uppercase tracking-wider">
-          Don't you have an account yet?
-        </h1>
-        <span>
-          <button
-            [routerLink]="['/register']"
-            class="w-full my-6 border-[1px] border-mainOrange rounded-md px-2 py-1 text-mainOrange hover:bg-mainOrange hover:text-mainGray">
-            Register now
-          </button>
-        </span>
-        <h1 class="text-xl pb-6 font-bold uppercase tracking-wider">
-          Why is it worth to create an account on RAG-2?
-        </h1>
-        <div class="flex flex-col space-y-8 text-mainOrange">
-          <span class="flex flex-row items-center space-x-4">
-            <i data-feather="save" class="size-8"></i>
-            <p>Save your games</p>
-          </span>
-          <span class="flex flex-row items-center space-x-4">
-            <i data-feather="link" class="size-8"></i>
-            <p>Keep everything together</p>
-          </span>
-          <span class="flex flex-row items-center space-x-4">
-            <i data-feather="package" class="size-8"></i>
-            <p>Watch log's console view</p>
-          </span>
-          <span class="flex flex-row items-center space-x-4">
-            <i data-feather="monitor" class="size-8"></i>
-            <p>Create your own AI models</p>
-          </span>
-        </div>
+        <app-side-form-panel
+          [mainText]="mainTextValue"
+          [buttonText]="'Register now'"
+          [routerLink]="'/register'" />
+        <app-benefits-list />
       </div>
     </div>
   `,
+  imports: [
+    ReactiveFormsModule,
+    LoginFormComponent,
+    BenefitsListComponent,
+    SideFormPanelComponent,
+  ],
 })
 export class LoginPageComponent implements AfterViewInit {
   public loginForm = new FormGroup({
     email: new FormControl(''),
     password: new FormControl(''),
   });
+
+  public mainTextValue = "Don't you have an account yet?";
 
   public ngAfterViewInit(): void {
     feather.replace();
