4a9e54afa3e30b71949fb5d27e327dccf89bfd1d
Marcin Bator <marcinbator.ofc@gmail.com>
2024-07-07 19:53:31 +0200

    feat: #13 basic socket workflow

--- src/app/game/components/ai-socket-menu/ai-socket-menu.component.ts
+++ src/app/game/components/ai-socket-menu/ai-socket-menu.component.ts
@@ -8,15 +8,72 @@ import { ILoggableDataComponent } from '../../models/loggable-data-component';
   imports: [],
   template: `
     <div class=" border-2 border-solid border-red-600 p-5">
-      ai-socket-menu works!
+      <input
+        class="border-2 border-solid border-black"
+        #socketDomain
+        type="text"
+        list="recentPhrases" />
+      <datalist id="recentPhrases">
+        @for (phrase of recentPhrases; track phrase) {
+          <option [value]="phrase"></option>
+        }
+      </datalist>
+      <div [textContent]="socketStatus"></div>
+      <button (click)="connect(socketDomain.value)">Connect</button>
+      <button (click)="socket?.close()">Disconnect</button>
+      <button (click)="send()">Send</button>
     </div>
   `,
 })
 export class AiSocketMenuComponent implements OnInit, ILoggableDataComponent {
   @Output() public logDataEmitter = new EventEmitter<TExchangeData>();
+
   public logData: TExchangeData = {};
+  public socket: WebSocket | null = null;
+  public recentPhrases: string[] = [];
+  public socketStatus = 'Disconnected';
 
   public ngOnInit(): void {
     this.logDataEmitter.emit(this.logData);
+    this.loadRecentPhrases();
+  }
+
+  public connect(socketDomain: string): void {
+    this.socket = new WebSocket(socketDomain);
+    this.socket.addEventListener('open', () => {
+      this.socketStatus = 'Connected';
+      this.saveRecentPhrase(socketDomain);
+    });
+    this.socket.addEventListener('message', event => {
+      console.log('Received message:', event.data);
+    });
+    this.socket.addEventListener('close', () => {
+      this.socketStatus = 'Disconnected';
+    });
+  }
+
+  public send(): void {
+    if (this.socket) {
+      this.socket.send(JSON.stringify({ message: 'Hello, World!' }));
+    }
+  }
+
+  //
+
+  private loadRecentPhrases(): void {
+    const cachedPhrases = localStorage.getItem('recentPhrases');
+    if (cachedPhrases) {
+      this.recentPhrases = JSON.parse(cachedPhrases);
+    }
+  }
+
+  private saveRecentPhrase(phrase: string): void {
+    if (!this.recentPhrases.includes(phrase)) {
+      this.recentPhrases.unshift(phrase);
+      if (this.recentPhrases.length > 5) {
+        this.recentPhrases.pop();
+      }
+      localStorage.setItem('recentPhrases', JSON.stringify(this.recentPhrases));
+    }
   }
 }
