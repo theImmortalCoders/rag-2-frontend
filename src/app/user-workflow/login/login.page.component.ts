f202906f38bac1b33098905c8a777e9181022818
Paweł Buczek <pbuczek7@wp.pl>
2024-07-08 19:52:11 +0200

    feat: #11 login-page rwd

--- src/app/user-workflow/login/login.page.component.ts
+++ src/app/user-workflow/login/login.page.component.ts
@@ -9,10 +9,11 @@ import { SideFormPanelComponent } from '../shared/components/side-form-panel/sid
   imports: [LoginFormComponent, BenefitsListComponent, SideFormPanelComponent],
   template: `
     <div
-      class="w-full min-h-screen bg-mainGray flex justify-evenly font-mono pt-20 text-mainCreme">
+      class="w-full min-h-screen bg-mainGray items-center lg:items-start flex flex-col lg:flex-row justify-center lg:justify-evenly font-mono pt-12 lg:pt-20 text-mainCreme">
       <app-login-form
-        class="w-1/3 h-fit border-2 border-mainOrange rounded-lg px-6 py-8" />
-      <div class="flex flex-col space-y-4 w-1/3 h-fit p-6">
+        class="w-4/5 xs:w-2/3 lg:w-1/3 h-fit border-2 border-mainOrange rounded-lg px-6 py-8" />
+      <div
+        class="flex flex-col md:flex-row lg:flex-col space-y-4 w-4/5 xs:w-2/3 md:w-4/5 lg:w-1/3 space-x-0 md:space-x-12 lg:space-x-0 h-fit p-6 pt-12 lg:pt-0">
         <app-side-form-panel
           [mainText]="mainTextValue"
           [buttonText]="'Register now'"
--- src/app/user-workflow/shared/components/side-form-panel/side-form-panel.component.ts
+++ src/app/user-workflow/shared/components/side-form-panel/side-form-panel.component.ts
@@ -6,7 +6,7 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterModule],
   template: `
-    <h1 class="text-2xl font-bold uppercase tracking-wider">
+    <h1 class="text-xl xs:text-2xl font-bold uppercase tracking-wider">
       {{ mainText }}
     </h1>
     <span>
