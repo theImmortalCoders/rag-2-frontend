9344f58ccf256e51aa76d3cadb20324eeb99580e
Paweł Buczek <pbuczek7@wp.pl>
2024-07-09 19:38:06 +0200

    feat: #11 added rwd to register page

--- src/app/user-workflow/register/register.page.component.ts
+++ src/app/user-workflow/register/register.page.component.ts
@@ -13,10 +13,11 @@ import { RegisterFormComponent } from './components/register-form.component';
   ],
   template: `
     <div
-      class="w-full min-h-screen bg-mainGray flex justify-evenly font-mono pt-20 text-mainCreme">
+      class="w-full min-h-screen bg-mainGray items-center lg:items-start flex flex-col lg:flex-row justify-center lg:justify-evenly font-mono pt-12 lg:pt-20 text-mainCreme">
       <app-register-form
-        class="w-1/3 h-fit border-2 border-mainOrange rounded-lg px-6 py-8" />
-      <div class="flex flex-col space-y-4 w-1/3 h-fit p-6">
+        class="w-4/5 xs:w-2/3 lg:w-1/3 h-fit border-2 border-mainOrange rounded-lg px-6 py-8" />
+      <div
+        class="flex flex-col md:flex-row lg:flex-col space-y-4 w-4/5 xs:w-2/3 md:w-4/5 lg:w-1/3 space-x-0 md:space-x-12 lg:space-x-0 h-fit p-6 pt-12 lg:pt-0">
         <app-side-form-panel
           [mainText]="'Do you have an account already?'"
           [buttonText]="'Log in'"
