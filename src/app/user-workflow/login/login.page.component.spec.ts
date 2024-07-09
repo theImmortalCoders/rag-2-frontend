169d372e2a67200413fc3d94ba335739ae8d3be2
Paweł Buczek <pbuczek7@wp.pl>
2024-07-09 19:53:38 +0200

    feat: #11 added some unit tests

--- src/app/user-workflow/login/login.page.component.spec.ts
+++ src/app/user-workflow/login/login.page.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginPageComponent } from './login.page.component';
+import { LoginFormComponent } from './components/login-form.component';
+import { BenefitsListComponent } from '../shared/components/benefits-list/benefits-list.component';
+import { SideFormPanelComponent } from '../shared/components/side-form-panel/side-form-panel.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
@@ -8,10 +12,17 @@ describe('LoginPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginPageComponent]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        LoginFormComponent,
+        BenefitsListComponent,
+        SideFormPanelComponent,
+      ],
+      declarations: [LoginPageComponent],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(LoginPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +30,29 @@ describe('LoginPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render LoginFormComponent', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('app-login-form')).not.toBeNull();
+  });
+
+  it('should render SideFormPanelComponent with correct inputs', () => {
+    const sideFormPanelElement = fixture.debugElement.query(
+      By.directive(SideFormPanelComponent)
+    );
+    expect(sideFormPanelElement).not.toBeNull();
+    const sideFormPanelInstance =
+      sideFormPanelElement.componentInstance as SideFormPanelComponent;
+    expect(sideFormPanelInstance.mainText).toBe(
+      "Don't you have an account yet?"
+    );
+    expect(sideFormPanelInstance.buttonText).toBe('Register now');
+    expect(sideFormPanelInstance.routerLink).toBe('/register');
+  });
+
+  it('should render BenefitsListComponent', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('app-benefits-list')).not.toBeNull();
   });
 });
--- src/app/user-workflow/register/register.page.component.spec.ts
+++ src/app/user-workflow/register/register.page.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegisterPageComponent } from './register.page.component';
+import { RegisterFormComponent } from './components/register-form.component';
+import { BenefitsListComponent } from '../shared/components/benefits-list/benefits-list.component';
+import { SideFormPanelComponent } from '../shared/components/side-form-panel/side-form-panel.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 describe('RegisterPageComponent', () => {
   let component: RegisterPageComponent;
@@ -8,10 +12,17 @@ describe('RegisterPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegisterPageComponent]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        RegisterFormComponent,
+        BenefitsListComponent,
+        SideFormPanelComponent,
+      ],
+      declarations: [RegisterPageComponent],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(RegisterPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +30,29 @@ describe('RegisterPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render RegisterFormComponent', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('app-register-form')).not.toBeNull();
+  });
+
+  it('should render SideFormPanelComponent with correct inputs', () => {
+    const sideFormPanelElement = fixture.debugElement.query(
+      By.directive(SideFormPanelComponent)
+    );
+    expect(sideFormPanelElement).not.toBeNull();
+    const sideFormPanelInstance =
+      sideFormPanelElement.componentInstance as SideFormPanelComponent;
+    expect(sideFormPanelInstance.mainText).toBe(
+      'Do you have an account already?'
+    );
+    expect(sideFormPanelInstance.buttonText).toBe('Log in');
+    expect(sideFormPanelInstance.routerLink).toBe('/login');
+  });
+
+  it('should render BenefitsListComponent', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('app-benefits-list')).not.toBeNull();
   });
 });
