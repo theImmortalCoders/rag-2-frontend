fd5d8d73780d22e38149b792d62820baf9ea1a77
Paweł Buczek <pbuczek7@wp.pl>
2024-07-19 00:21:28 +0200

    feat: #18 added some rwd and tests

--- src/app/game/components/console/console.component.ts
+++ src/app/game/components/console/console.component.ts
@@ -8,12 +8,16 @@ import { ExchangeDataPipe } from '../../../../utils/pipes/exchange-data.pipe';
   standalone: true,
   template: `
     @for (data of logData | keyvalue; track data.key) {
-      <div class="ml-4 max-w-72 overflow-y-auto">
-        <span class="font-bold text-lg font-mono">{{ data.key }}:</span>
+      <div class="ml-4 max-w-60 lg:max-w-72 overflow-y-auto">
+        <span class="font-bold text-base xl:text-lg font-mono"
+          >{{ data.key }}:</span
+        >
         @if (isTLogData(data.value)) {
           <app-console [logData]="data.value | exchange_data" />
         } @else {
-          <span class="ml-2 text-base text-mainCreme">{{ data.value }}</span>
+          <span class="ml-2 text-sm xl:text-base text-mainCreme">{{
+            data.value
+          }}</span>
         }
       </div>
     }
