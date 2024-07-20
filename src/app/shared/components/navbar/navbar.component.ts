4c1a5a9b5d7e423a5b541004e1bf1e7bcdbb0294
Paweł Buczek <pbuczek7@wp.pl>
2024-07-20 17:53:25 +0200

    feat: #22 added logo to navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, AfterViewInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import * as feather from 'feather-icons';
@@ -5,15 +6,19 @@ import * as feather from 'feather-icons';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgOptimizedImage],
   template: `
     <nav
       class="bg-darkGray min-w-max pl-6 py-4 shadow-navbarShadow relative z-50">
       <div class="flex mx-auto w-full flex-row items-center justify-between">
         <div
           class="flex w-1/2 xs:w-[60%] sm:w-5/12 lg:w-1/3 justify-between items-center">
-          <a [routerLink]="['/']">
-            <img src="" alt="Logo" class="h-12 w-auto" />
+          <a [routerLink]="['/']" class="h-12 w-12 relative">
+            <img
+              ngSrc="images/rag-2.png"
+              alt="Logo"
+              class="object-contain"
+              fill />
           </a>
           <span class="text-3xl font-mono text-mainOrange hidden xs:block"
             >RAG-2</span
