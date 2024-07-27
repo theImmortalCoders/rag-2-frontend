451a6789bcb614f15e2f7657c130bcaabe942a96
Paweł Buczek <pbuczek7@wp.pl>
2024-07-27 15:50:31 +0200

    feat: #27 navbar first improvements

---
+++ src/app/shared/components/navbar/game-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-list',
+  standalone: true,
+  imports: [],
+  template: `
+    <ul class="text-lg space-y-2 px-2 pb-2">
+      <li>aaa</li>
+      <li>bbbbbbbbbbbbbbbbbbbbbbbb</li>
+      <li>ccc</li>
+      <li>ddd</li>
+    </ul>
+  `,
+})
+export class GameListComponent {}
