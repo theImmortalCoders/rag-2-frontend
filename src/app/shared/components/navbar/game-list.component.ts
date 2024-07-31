d3857a85f8d39d1d0eeeaff0ba92ab0e5190409c
Paweł Buczek <pbuczek7@wp.pl>
2024-07-31 14:35:27 +0200

    feat: #27 navbar improvements

--- src/app/shared/components/navbar/game-list.component.ts
+++ src/app/shared/components/navbar/game-list.component.ts
@@ -5,14 +5,35 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <ul class="text-lg space-y-2 px-2 pb-2">
-      <li>PONG</li>
-      <li>TIC TAC TOE</li>
-      <li>ARKANOID</li>
-      <li>SNAKE</li>
-      <li>FLAPPY BIRD</li>
-      <li>SPACE RUSH</li>
+    <ul class="text-lg space-y-2 px-2 pb-4">
+      <li class="w-full group">
+        <span>PONG</span>
+        <hr class="{{ liStyle }}" />
+      </li>
+      <li class="w-full group">
+        <span>TIC TAC TOE</span>
+        <hr class="{{ liStyle }}" />
+      </li>
+      <li class="w-full group">
+        <span>ARKANOID</span>
+        <hr class="{{ liStyle }}" />
+      </li>
+      <li class="w-full group">
+        <span>SNAKE</span>
+        <hr class="{{ liStyle }}" />
+      </li>
+      <li class="w-full group">
+        <span>FLAPPY BIRD</span>
+        <hr class="{{ liStyle }}" />
+      </li>
+      <li class="w-full group">
+        <span>SPACE RUSH</span>
+        <hr class="{{ liStyle }} durat" />
+      </li>
     </ul>
   `,
 })
-export class GameListComponent {}
+export class GameListComponent {
+  public liStyle =
+    'border-mainOrange w-2/5 group-hover:w-full ease-in-out transition-all duration-500';
+}
