78fd87fe19dc9b904c9daff00bc10146a038a713
Paweł Buczek <pbuczek7@wp.pl>
2024-07-31 16:06:55 +0200

    feat: #27 handle game list with all functionalities

--- src/app/shared/components/navbar/game-list.component.ts
+++ src/app/shared/components/navbar/game-list.component.ts
@@ -1,39 +1,36 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-game-list',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   template: `
-    <ul class="text-lg space-y-2 px-2 pb-4">
-      <li class="w-full group">
-        <span>PONG</span>
-        <hr class="{{ liStyle }}" />
-      </li>
-      <li class="w-full group">
-        <span>TIC TAC TOE</span>
-        <hr class="{{ liStyle }}" />
-      </li>
-      <li class="w-full group">
-        <span>ARKANOID</span>
-        <hr class="{{ liStyle }}" />
-      </li>
-      <li class="w-full group">
-        <span>SNAKE</span>
-        <hr class="{{ liStyle }}" />
-      </li>
-      <li class="w-full group">
-        <span>FLAPPY BIRD</span>
-        <hr class="{{ liStyle }}" />
-      </li>
-      <li class="w-full group">
-        <span>SPACE RUSH</span>
-        <hr class="{{ liStyle }} durat" />
-      </li>
+    <ul class="text-lg space-y-2 px-2 pt-2 pb-4">
+      @for (game of games; track game.name) {
+        <li class="w-full group">
+          <a
+            [routerLink]="['game/', game.url]"
+            class="flex flex-row justify-between items-center">
+            <span>{{ game.name }}</span>
+            <i
+              data-feather="corner-down-right"
+              class="size-5 ease-in-out transition-all duration-500 opacity-0 group-hover:opacity-100"></i>
+          </a>
+          <hr
+            class="border-mainOrange w-2/5 group-hover:w-full ease-in-out transition-all duration-500" />
+        </li>
+      }
     </ul>
   `,
 })
 export class GameListComponent {
-  public liStyle =
-    'border-mainOrange w-2/5 group-hover:w-full ease-in-out transition-all duration-500';
+  public games = [
+    { name: 'PONG', url: 'pong' },
+    { name: 'TIC TAC TOE', url: 'tictactoe' },
+    { name: 'ARKANOID', url: 'arkanoid' },
+    { name: 'SNAKE', url: 'snake' },
+    { name: 'FLAPPY BIRD', url: 'flappybird' },
+    { name: 'SPACE RUSH', url: 'spacerush' },
+  ];
 }
