c9b4d6f630e5ac18a4ef0f5a9d10a72cb94189c8
Paweł Buczek <pbuczek7@wp.pl>
2024-08-02 19:30:48 +0200

    feat: #27 finish of navbar rwd + other features

--- src/app/shared/components/navbar/game-list.component.ts
+++ src/app/shared/components/navbar/game-list.component.ts
@@ -6,22 +6,25 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterModule],
   template: `
-    <ul class="text-lg space-y-2 px-4 py-4">
+    <ul class="text-base xs:text-lg space-y-2 px-4 py-4">
       @for (game of games; track game.name) {
         <li class="w-full group">
           <a
             [routerLink]="['game/', game.url]"
             class="flex flex-row justify-between items-center">
-            <span>{{ game.name }}</span>
+            <span
+              class="text-mainOrange group-hover:text-green-500 ease-in-out transition-all duration-500"
+              >{{ game.name }}</span
+            >
             <i
               data-feather="corner-down-right"
-              class="size-5 ease-in-out transition-all duration-500 opacity-0 group-hover:opacity-100"></i>
+              class="size-4 xs:size-5 ease-in-out transition-all duration-500 opacity-0 group-hover:opacity-100 text-mainOrange group-hover:text-green-500"></i>
           </a>
           <hr
-            class="border-mainOrange w-3/5 2xl:w-2/5 group-hover:w-full ease-in-out transition-all duration-500" />
+            class="border-mainOrange group-hover:border-green-500 w-3/5 2xl:w-2/5 group-hover:w-full ease-in-out transition-all duration-500" />
         </li>
       }
-      <li class="text-base text-start 3xl:text-center">
+      <li class="text-sm xs:text-base text-start 3xl:text-center">
         Read more about our games...
       </li>
     </ul>
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -11,47 +11,51 @@ import * as feather from 'feather-icons';
   imports: [RouterModule, NgOptimizedImage, GameListComponent],
   template: `
     <nav
-      class="bg-mainGray min-w-max pl-6 py-4 shadow-navbarShadow relative z-50">
+      class="bg-mainGray min-w-max pl-3 xs:pl-6 py-4 shadow-navbarShadow relative z-50">
       <div
         class="flex mx-auto w-full flex-row items-center justify-between font-mono text-mainOrange">
-        <div class="flex w-1/4 lg:w-1/2 justify-between items-center">
-          <a [routerLink]="['/']" class="size-12 relative -rotate-6">
+        <div class="flex w-fit md:w-1/4 lg:w-1/2 justify-between items-center">
+          <a
+            [routerLink]="['/']"
+            class="size-10 2xs:size-12 relative -rotate-6">
             <img
               ngSrc="images/rag-2.png"
               alt="Logo"
               class="object-contain"
               fill />
           </a>
-          <span class="text-3xl hidden xs:block">{{
+          <span class="text-3xl hidden md:block">{{
             isMinWidthLg ? 'RUT-AI-GAMES 2' : 'RAG-2'
           }}</span>
         </div>
         <div
-          class="text-2xl flex flex-col w-[30%] lg:w-1/4 xl:w-1/6 relative items-center justify-center">
+          class="text-xl xs:text-2xl flex flex-col w-3/5 2xs:w-1/2 sm:w-2/5 md:w-[30%] lg:w-1/4 xl:w-1/6 relative items-center justify-center">
           <button
-            class="flex flex-row w-full items-center justify-center space-x-2 p-1 relative z-40 border-b-2 border-mainOrange hover:border-green-500"
+            class="flex flex-row w-full items-center justify-center space-x-1 xs:space-x-2 p-1 relative z-40 border-b-[1px] xs:border-b-2 border-mainOrange hover:border-green-500 ease-in-out transition-all duration-500"
             (click)="toggleGameList()">
-            <div
-              class="ease-in-out transition-all duration-200 {{
-                isGameListActive ? '-rotate-90' : 'rotate-0'
-              }}">
-              <i data-feather="chevrons-down" class="size-6"></i>
-            </div>
-            <span>LET'S PLAY</span>
             <div
               class="ease-in-out transition-all duration-200 {{
                 isGameListActive ? 'rotate-90' : 'rotate-0'
               }}">
-              <i data-feather="chevrons-down" class="size-6"></i>
+              <i data-feather="chevrons-right" class="size-5 xs:size-6"></i>
+            </div>
+            <span>LET'S PLAY</span>
+            <div
+              class="ease-in-out transition-all duration-200 {{
+                isGameListActive ? '-rotate-90' : 'rotate-0'
+              }}">
+              <i data-feather="chevrons-left" class="size-5 xs:size-6"></i>
             </div>
           </button>
           <app-game-list
             class="absolute z-20 w-full bg-mainGray ease-in-out transition-all duration-200 shadow-navbarShadow rounded-b-lg {{
-              isGameListActive ? 'top-[64px] opacity-100' : '-top-72 opacity-0'
+              isGameListActive
+                ? 'top-[56px] 2xs:top-[60px] xs:top-[64px] opacity-100'
+                : '-top-72 opacity-0'
             }}" />
         </div>
-        <a [routerLink]="['/login']" class="mr-10"
-          ><i data-feather="users" class="size-9"></i
+        <a [routerLink]="['/login']" class="mr-5 xs:mr-10"
+          ><i data-feather="users" class="size-8 2xs:size-9"></i
         ></a>
       </div>
     </nav>
