9af26704c8679231b55f0fdddbb132183380c9c3
Paweł Buczek <pbuczek7@wp.pl>
2024-08-17 14:40:07 +0200

    feat: #25 created a general plan of work

--- src/app/user-workflow/login/services/login.service.ts
+++ src/app/user-workflow/login/services/login.service.ts
@@ -9,6 +9,8 @@ import { environment } from '@env/environment';
 export class LoginService {
   private _httpClient = inject(HttpClient);
 
+  //tu bedzie wywolywanie endpointow z /shared, czyli tam napisany url do backendu itd a tu wywolanie + obsluga np localStorage (aktualnie w komponencie)
+
   public authenticateUser(email: string, password: string): Observable<string> {
     return this._httpClient.post<string>(
       environment.backendApiUrl + '/api/user',
