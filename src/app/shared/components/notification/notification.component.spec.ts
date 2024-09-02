a7dc0ea9f8b592dcba2413b5586a4654e0ebf879
Paweł Buczek <pbuczek7@wp.pl>
2024-09-02 20:09:26 +0200

    refactor: #49 reorganized notifications, added animations and tests

--- src/app/shared/components/notification/notification.component.spec.ts
+++ src/app/shared/components/notification/notification.component.spec.ts
@@ -1,78 +1,80 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NotificationComponent } from './notification.component';
 import { NotificationService } from 'app/shared/services/notification.service';
-import { of } from 'rxjs';
+import { Renderer2 } from '@angular/core';
 import { INotification } from 'app/shared/models/notification';
+import { Subject } from 'rxjs';
 
 describe('NotificationComponent', () => {
   let component: NotificationComponent;
   let fixture: ComponentFixture<NotificationComponent>;
-  let notificationService: jasmine.SpyObj<NotificationService>;
-
-  const mockNotifications: INotification[] = [
-    { message: 'Test notification 1', dismissible: true },
-    { message: 'Test notification 2', dismissible: false },
-  ];
+  let notificationService: NotificationService;
+  let _renderer2: Renderer2;
 
   beforeEach(async () => {
-    const notificationServiceSpy = jasmine.createSpyObj('NotificationService', [
-      'notifications$',
-      'removeNotification',
-    ]);
-    notificationServiceSpy.notifications$ = of(mockNotifications);
+    const notificationServiceMock = {
+      notifications$: new Subject<INotification[]>(),
+      notificationRemoval$: new Subject<INotification>(),
+      triggerRemoveNotification: jasmine.createSpy('triggerRemoveNotification'),
+      removeNotification: jasmine.createSpy('removeNotification'),
+    };
 
     await TestBed.configureTestingModule({
-      imports: [NotificationComponent],
+      imports: [NotificationComponent], // Importujemy zamiast deklarować
       providers: [
-        { provide: NotificationService, useValue: notificationServiceSpy },
+        { provide: NotificationService, useValue: notificationServiceMock },
+        Renderer2,
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NotificationComponent);
     component = fixture.componentInstance;
-
-    notificationService = TestBed.inject(
-      NotificationService
-    ) as jasmine.SpyObj<NotificationService>;
+    notificationService = TestBed.inject(NotificationService);
+    _renderer2 = TestBed.inject(Renderer2);
 
     fixture.detectChanges();
   });
 
-  it('should create the component', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should subscribe to notifications and display them', () => {
-    expect(component.notifications).toEqual(mockNotifications);
+  it('should subscribe to notifications and update the notifications array', () => {
+    const notifications: INotification[] = [
+      { id: 1, message: 'Test notification 1', dismissible: true },
+      { id: 2, message: 'Test notification 2', dismissible: false },
+    ];
 
-    const notificationElements =
-      fixture.nativeElement.querySelectorAll('div p');
-    expect(notificationElements.length).toBe(2);
-    expect(notificationElements[0].textContent).toContain(
-      'Test notification 1'
+    (notificationService.notifications$ as Subject<INotification[]>).next(
+      notifications
     );
-    expect(notificationElements[1].textContent).toContain(
-      'Test notification 2'
-    );
+
+    expect(component.notifications).toEqual(notifications);
   });
 
-  it('should remove a notification when the remove button is clicked', () => {
-    const removeButton = fixture.nativeElement.querySelector('button');
-    removeButton.click();
-
-    expect(notificationService.removeNotification).toHaveBeenCalledWith(
-      mockNotifications[0]
-    );
-  });
-
-  it('should unsubscribe from notifications on component destroy', () => {
+  it('should unsubscribe from notifications on destroy', () => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     spyOn(component['_notificationSubscription']!, 'unsubscribe');
-
     component.ngOnDestroy();
-
     expect(
       component['_notificationSubscription']?.unsubscribe
     ).toHaveBeenCalled();
+  });
+
+  it('should call triggerRemoveNotification when button is clicked', () => {
+    const notification: INotification = {
+      id: 1,
+      message: 'Test notification',
+      dismissible: true,
+    };
+    component.notifications = [notification];
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+
+    expect(notificationService.triggerRemoveNotification).toHaveBeenCalledWith(
+      notification
+    );
   });
 });
--- src/app/shared/components/notification/notification.component.ts
+++ src/app/shared/components/notification/notification.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { INotification } from 'app/shared/models/notification';
 import { NotificationService } from 'app/shared/services/notification.service';
 import { Subscription } from 'rxjs';
@@ -7,14 +7,16 @@ import { Subscription } from 'rxjs';
   selector: 'app-notification',
   standalone: true,
   template: `
-    <div class="fixed w-1/6 bottom-0 left-0 z-50 font-mono">
-      @for (notification of notifications; track notification) {
+    <div
+      class="fixed text-sm xs:text-base w-3/5 xs:w-1/2 sm:w-2/5 md:w-1/3 lg:w-1/4 xl:w-1/6 bottom-0 left-0 z-50 font-mono">
+      @for (notification of notifications; track notification.id) {
         <div
-          class="bg-lightGray border-[1px] border-mainOrange rounded-md text-white p-4 m-2 text-center">
+          [id]="'notification-' + notification.id"
+          class="bg-lightGray border-[1px] border-mainOrange rounded-md text-white p-4 m-2 text-center transition-all ease-in-out duration-200 opacity-100">
           <p>{{ notification.message }}</p>
           @if (notification.dismissible) {
             <button
-              (click)="removeNotification(notification)"
+              (click)="triggerRemoveNotification(notification)"
               class="border-mainOrange border-[1px] w-3/4 font-bold text-mainOrange px-2 py-1 mt-2 rounded transition-all ease-in-out hover:bg-mainOrange hover:text-mainGray">
               OK
             </button>
@@ -26,6 +28,7 @@ import { Subscription } from 'rxjs';
 })
 export class NotificationComponent implements OnInit, OnDestroy {
   private _notificationService = inject(NotificationService);
+  private _renderer = inject(Renderer2);
 
   private _notificationSubscription: Subscription | null = null;
 
@@ -36,10 +39,36 @@ export class NotificationComponent implements OnInit, OnDestroy {
       this._notificationService.notifications$.subscribe(notifications => {
         this.notifications = notifications;
       });
+
+    this._notificationService.notificationRemoval$.subscribe(notification => {
+      const container = this.findNotificationContainer(notification.id);
+      if (container) {
+        this.removeNotification(notification, container);
+      }
+    });
   }
 
-  public removeNotification(notification: INotification): void {
-    this._notificationService.removeNotification(notification);
+  private findNotificationContainer(
+    notificationId: number
+  ): HTMLDivElement | null {
+    return document.querySelector(
+      `#notification-${notificationId}`
+    ) as HTMLDivElement;
+  }
+
+  public removeNotification(
+    notification: INotification,
+    container: HTMLDivElement
+  ): void {
+    this._renderer.removeClass(container, 'opacity-100');
+    this._renderer.addClass(container, 'opacity-0');
+    setTimeout(() => {
+      this._notificationService.removeNotification(notification);
+    }, 200);
+  }
+
+  public triggerRemoveNotification(notification: INotification): void {
+    this._notificationService.triggerRemoveNotification(notification);
   }
 
   public ngOnDestroy(): void {
