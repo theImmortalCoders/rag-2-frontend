983642f2e4e225252010cd508279b2390880421c
Paweł Buczek <pbuczek7@wp.pl>
2024-10-16 22:24:33 +0200

    feat: #40 added rwd to menu and link to dashboard

--- src/app/shared/components/navbar/components/user-shortcut/user-shortcut.component.ts
+++ src/app/shared/components/navbar/components/user-shortcut/user-shortcut.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { UserEndpointsService } from '@endpoints/user-endpoints.service';
 import { TRole } from 'app/shared/models/role.enum';
 import { AuthenticationService } from 'app/shared/services/authentication.service';
@@ -9,7 +9,7 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-user-shortcut',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   template: `
     <button (click)="handleButtonClick()" class="mr-5 xs:mr-10">
       <i
@@ -17,24 +17,26 @@ import { Subscription } from 'rxjs';
         class="hover:text-green-500 ease-in-out transition-all duration-500 size-8 2xs:size-9"></i>
     </button>
     <div
-      class="flex flex-col space-y-1 text-base w-56 absolute -z-10 transition-all ease-in-out duration-300 {{
+      class="flex flex-col space-y-1 text-sm 2xs:text-base w-48 md:w-52 lg:w-44 xl:w-56 absolute -z-10 transition-all ease-in-out duration-300 {{
         isUserInfoVisible ? 'right-0 opacity-100' : '-right-20 opacity-0'
-      }} top-[83px] bg-mainGray shadow-userInfoShadow p-3 rounded-bl-lg">
+      }} top-[75px] 2xs:top-[83px] bg-mainGray shadow-userInfoShadow p-3 rounded-bl-lg">
       <span class="text-center text-lightOragne font-bold"
         >Your role: {{ currentUserRole }}</span
       >
       <hr class="border-[1px] border-lightOragne" />
-      <button
+      <a
+        [routerLink]="['dashboard']"
+        (click)="isUserInfoVisible = false"
         class="flex flex-row items-center justify-center space-x-2 text-center hover:text-green-500 ease-in-out transition-all duration-500">
         <span>DASHBOARD</span>
-        <i data-feather="grid" class="size-4"></i>
-      </button>
+        <i data-feather="grid" class="size-3 2xs:size-4"></i>
+      </a>
       <hr class="border-mainOrange" />
       <button
         (click)="logoutButtonClick()"
         class="flex flex-row items-center justify-center space-x-2 text-center hover:text-green-500 ease-in-out transition-all duration-500">
         <span>LOGOUT</span>
-        <i data-feather="log-out" class="size-4"></i>
+        <i data-feather="log-out" class="size-3 2xs:size-4"></i>
       </button>
     </div>
   `,
