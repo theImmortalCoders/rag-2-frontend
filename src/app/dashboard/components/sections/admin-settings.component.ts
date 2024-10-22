d29641725685f8400ce5e5eb4d38ff532600ea19
Paweł Buczek <pbuczek7@wp.pl>
2024-10-22 22:39:37 +0200

    feat: #40 prepared admin settings section

--- src/app/dashboard/components/sections/admin-settings.component.ts
+++ src/app/dashboard/components/sections/admin-settings.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ModalComponent } from '../modal.component';
+import { AdministrationEndpointsService } from '@endpoints/administration-endpoints.service';
+import { NotificationService } from 'app/shared/services/notification.service';
+import { NonNullableFormBuilder } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { IUserResponse } from 'app/shared/models/user.models';
 
 @Component({
   selector: 'app-admin-settings',
@@ -11,6 +16,7 @@ import { ModalComponent } from '../modal.component';
     <div class="flex flex-row justify-around space-x-8">
       <button
         type="button"
+        (click)="banUnbanUserModal()"
         class="flex flex-row items-center justify-center group space-x-2 rounded-lg mt-2 px-3 py-2 bg-mainGray text-mainOrange border-2 border-mainOrange transition-all ease-in-out hover:bg-mainOrange hover:text-mainGray text-base">
         <span>Ban/unban some user</span>
         <i
@@ -19,6 +25,7 @@ import { ModalComponent } from '../modal.component';
       </button>
       <button
         type="button"
+        (click)="changeUserRoleModal()"
         class="flex flex-row items-center justify-center group space-x-2 rounded-lg mt-2 px-3 py-2 bg-mainGray text-mainOrange border-2 border-mainOrange transition-all ease-in-out hover:bg-mainOrange hover:text-mainGray text-base">
         <span>Change role of some user</span>
         <i
@@ -27,18 +34,11 @@ import { ModalComponent } from '../modal.component';
       </button>
       <button
         type="button"
+        (click)="getUserDetailsModal()"
         class="flex flex-row items-center justify-center group space-x-2 rounded-lg mt-2 px-3 py-2 bg-mainGray text-mainOrange border-2 border-mainOrange transition-all ease-in-out hover:bg-mainOrange hover:text-mainGray text-base">
         <span>Get details of some user</span>
         <i
           data-feather="eye"
-          class="text-mainOrange group-hover:text-mainGray transition-all ease-in-out size-4 xs:size-5"></i>
-      </button>
-      <button
-        type="button"
-        class="flex flex-row items-center justify-center group space-x-2 rounded-lg mt-2 px-3 py-2 bg-mainGray text-mainOrange border-2 border-mainOrange transition-all ease-in-out hover:bg-mainOrange hover:text-mainGray text-base">
-        <span>Get students lists</span>
-        <i
-          data-feather="list"
           class="text-mainOrange group-hover:text-mainGray transition-all ease-in-out size-4 xs:size-5"></i>
       </button>
     </div>
@@ -48,12 +48,27 @@ import { ModalComponent } from '../modal.component';
           <h2 class="text-3xl text-mainCreme font-bold mb-10">
             {{ modalTitle }}
           </h2>
-
-          <button
-            class="flex flex-row w-full items-center justify-center group space-x-2 rounded-lg mt-6 px-3 py-2 bg-mainGray text-mainOrange border-2 border-mainOrange transition-all ease-in-out hover:bg-mainOrange hover:text-mainGray text-base"
-            (click)="modalButtonFunction()">
-            {{ modalButtonText }}
-          </button>
+          <div class="flex flex-col space-y-4 w-full">
+            <select
+              id="selectedUserId"
+              class="custom-input"
+              value=""
+              (change)="setSelectedUserId($event)">
+              <option value="0">Choose one of the users</option>
+              @for (user of usersList; track user.id) {
+                <option [value]="user.id">
+                  {{ user.name }}, {{ user.email }}
+                </option>
+              }
+            </select>
+          </div>
+          @if (modalButtonText !== null) {
+            <button
+              class="flex flex-row w-full items-center justify-center group space-x-2 rounded-lg mt-6 px-3 py-2 bg-mainGray text-mainOrange border-2 border-mainOrange transition-all ease-in-out hover:bg-mainOrange hover:text-mainGray text-base"
+              (click)="modalButtonFunction()">
+              {{ modalButtonText }}
+            </button>
+          }
           <button
             (click)="hideModal()"
             class="absolute top-2 right-4 text-5xl text-mainOrange hover:text-mainGray">
@@ -70,17 +85,75 @@ import { ModalComponent } from '../modal.component';
   `,
 })
 export class AdminSettingsComponent {
+  private _adminEndpointsService = inject(AdministrationEndpointsService);
+  private _notificationService = inject(NotificationService);
+
+  private _getUsersSubscription: Subscription | null = null;
+
+  public selectedUserId = 0;
+
+  public usersList: IUserResponse[] | null = null;
+
   public errorMessage: string | null = null;
 
   public modalVisibility:
     | 'banUnbanUser'
     | 'changeUserRole'
     | 'getUserDetails'
-    | 'getStudentsList'
     | null = null;
   public modalTitle = '';
-  public modalButtonText = '';
+  public modalButtonText: string | null = '';
   public modalButtonFunction!: () => void;
+
+  public setSelectedUserId(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    const selectedId = target?.value;
+    this.selectedUserId = parseInt(selectedId, 10);
+  }
+
+  public getUsersList(): void {
+    this._getUsersSubscription = this._adminEndpointsService
+      .getUsers()
+      .subscribe({
+        next: (response: IUserResponse[]) => {
+          this.usersList = response;
+        },
+      });
+  }
+
+  public banUnbanUserModal(): void {
+    this.modalVisibility = 'banUnbanUser';
+    this.modalTitle = 'Changing ban status of user';
+    this.modalButtonText = 'Change ban status';
+    this.modalButtonFunction = this.banUnbanUserFunction;
+    this.errorMessage = null;
+    this.getUsersList();
+  }
+
+  public changeUserRoleModal(): void {
+    this.modalVisibility = 'changeUserRole';
+    this.modalTitle = 'Changing user role';
+    this.modalButtonText = 'Change user role';
+    this.modalButtonFunction = this.changeUserRoleFunction;
+    this.errorMessage = null;
+    this.getUsersList();
+  }
+
+  public getUserDetailsModal(): void {
+    this.modalVisibility = 'getUserDetails';
+    this.modalTitle = 'Checking user details';
+    this.modalButtonText = null;
+    this.errorMessage = null;
+    this.getUsersList();
+  }
+
+  public banUnbanUserFunction(): void {
+    //
+  }
+
+  public changeUserRoleFunction(): void {
+    //
+  }
 
   public hideModal(): void {
     this.modalVisibility = null;
