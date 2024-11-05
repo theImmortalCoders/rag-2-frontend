f3f6787baa6027242fb0373686b1604117f9125a
Paweł Buczek <pbuczek7@wp.pl>
2024-11-05 16:59:49 +0100

    feat: #92 getting storage limits from backend

--- src/app/dashboard/components/sections/user-info/user-info.component.ts
+++ src/app/dashboard/components/sections/user-info/user-info.component.ts
@@ -70,7 +70,7 @@ import { ProgressCircleBarComponent } from './progress-circle-bar.component';
       <app-progress-circle-bar
         class="flex items-center justify-center w-full lg:w-1/3 xl:w-1/4 pr-0 lg:pr-32 pt-8 lg:pt-4"
         [usedSpace]="userStatsInfo?.totalStorageMb"
-        [totalSpace]="10.0" />
+        [currentUserRole]="aboutMeUserInfo?.role" />
     </div>
   `,
 })
