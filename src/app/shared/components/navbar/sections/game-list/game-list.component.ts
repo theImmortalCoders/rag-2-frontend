b64faef9abc371f0a05f97e96050ceb0a6cc3ffe
Paweł Buczek <pbuczek7@wp.pl>
2024-11-07 15:43:41 +0100

    feat: #dev get game list to navbar from backend

--- src/app/shared/components/navbar/sections/game-list/game-list.component.ts
+++ src/app/shared/components/navbar/sections/game-list/game-list.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { GameEndpointsService } from '@endpoints/game-endpoints.service';
+import { IGameResponse } from 'app/shared/models/game.models';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-game-list',
@@ -7,13 +10,13 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule],
   template: `
     <ul class="text-base xs:text-lg space-y-2 px-4 py-4">
-      @for (game of games; track game.name) {
+      @for (game of games; track game.id) {
         <li class="w-full group">
           <a
-            [routerLink]="['game/', game.url]"
+            [routerLink]="['game/', game.name]"
             class="flex flex-row justify-between items-center">
             <span
-              class="text-mainOrange group-hover:text-green-500 ease-in-out transition-all duration-500"
+              class="uppercase text-mainOrange group-hover:text-green-500 ease-in-out transition-all duration-500"
               >{{ game.name }}</span
             >
             <i
@@ -21,7 +24,7 @@ import { RouterModule } from '@angular/router';
               class="size-4 xs:size-5 ease-in-out transition-all duration-500 opacity-0 group-hover:opacity-100 text-mainOrange group-hover:text-green-500"></i>
           </a>
           <hr
-            class="border-mainOrange group-hover:border-green-500 w-3/5 2xl:w-2/5 group-hover:w-full ease-in-out transition-all duration-500" />
+            class="border-mainOrange group-hover:border-green-500 w-3/5 2xl:w-1/2 group-hover:w-full ease-in-out transition-all duration-500" />
         </li>
       }
       <li class="text-sm xs:text-base text-start 3xl:text-center">
@@ -30,13 +33,27 @@ import { RouterModule } from '@angular/router';
     </ul>
   `,
 })
-export class GameListComponent {
-  public games = [
-    { name: 'PONG', url: 'pong' },
-    { name: 'TIC TAC TOE', url: 'tictactoe' },
-    { name: 'ARKANOID', url: 'arkanoid' },
-    { name: 'SNAKE', url: 'snake' },
-    { name: 'FLAPPY BIRD', url: 'flappybird' },
-    { name: 'SPACE RUSH', url: 'spacerush' },
-  ];
+export class GameListComponent implements OnInit, OnDestroy {
+  private _gameEndpointsService = inject(GameEndpointsService);
+
+  private _getGamesSubscription = new Subscription();
+
+  public games: IGameResponse[] | null = null;
+
+  public ngOnInit(): void {
+    this._getGamesSubscription = this._gameEndpointsService
+      .getGames()
+      .subscribe({
+        next: (response: IGameResponse[]) => {
+          this.games = response;
+        },
+        error: () => {
+          this.games = null;
+        },
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this._getGamesSubscription.unsubscribe();
+  }
 }
