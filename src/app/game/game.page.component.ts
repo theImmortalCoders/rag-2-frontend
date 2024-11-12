c255a4c2a17f925951ef58b3b6f7f4c68fe21ec0
Paweł Buczek <pbuczek7@wp.pl>
2024-11-12 18:58:02 +0100

    refactor: #56 divide into separate component

--- src/app/game/game.page.component.ts
+++ src/app/game/game.page.component.ts
@@ -16,7 +16,7 @@ import { GameMenuComponent } from './components/game-menu/game-menu.component';
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { CantDisplayGameComponent } from './components/cant-display-game/cant-display-game.component';
 import { SkiJumpGameWindowComponent } from './games/ski-jump/ski-jump.component';
-import { CommonModule } from '@angular/common';
+import { GameControlsComponent } from './components/game-controls/game-controls.component';
 
 @Component({
   selector: 'app-game',
@@ -32,7 +32,7 @@ import { CommonModule } from '@angular/common';
     CantDisplayGameComponent,
     AuthRequiredDirective,
     SkiJumpGameWindowComponent,
-    CommonModule,
+    GameControlsComponent,
   ],
   template: `
     <div class="flex flex-col min-h-all w-full items-center bg-gray-400">
@@ -66,36 +66,7 @@ import { CommonModule } from '@angular/common';
                 [setDataPossibleToPersist]="gameStateData" />
             </div>
           </div>
-          <div class="group font-mono absolute left-0 top-0 z-30">
-            <div
-              class="absolute z-30 top-4 left-4 rounded-full bg-gray-400 group-hover:bg-mainCreme border-2 border-mainGray transition-all ease-in-out duration-300">
-              <i
-                data-feather="info"
-                class="size-9 text-mainGray group-hover:scale-110 transition-all ease-in-out duration-300"></i>
-            </div>
-            <div
-              class="flex absolute z-20 top-4 left-4 h-10 w-56 pointer-events-none opacity-0 group-hover:opacity-100 items-start justify-center rounded-l-full rounded-tr-full bg-mainGray text-mainCreme text-nowrap transition-all ease-in-out duration-300">
-              <p class="text-center py-2 pl-12 pr-4 uppercase">
-                Game controls:
-              </p>
-            </div>
-            <div
-              class="flex flex-col w-[184px] absolute z-10 top-14 left-14 pl-3 pb-4 shadow-controlPanelShadow pointer-events-none opacity-0 group-hover:opacity-100 bg-mainGray text-mainCreme transition-all ease-in-out duration-300">
-              @for (player of game.players; track player) {
-                <span class="text-bold text-sm uppercase text-mainOrange"
-                  >Player {{ $index + 1 }}</span
-                >
-                @for (
-                  control of player.controlsDescription | keyvalue;
-                  track control
-                ) {
-                  <span class="text-xs pl-2"
-                    >{{ control.key }}: {{ control.value }}</span
-                  >
-                }
-              }
-            </div>
-          </div>
+          <app-game-controls [game]="game" />
           <div class="flex w-full items-center justify-center py-12">
             @switch (game.name) {
               @case ('pong') {
