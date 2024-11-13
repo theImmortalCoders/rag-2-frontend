6d1005c4ddea12ab90624b600ad7272b7af7f0de
Paweł Buczek <pbuczek7@wp.pl>
2024-11-13 20:39:40 +0100

    feat: #123 added global comments

--- src/app/game/games/flappy-bird/flappy-bird.component.ts
+++ src/app/game/games/flappy-bird/flappy-bird.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { CanvasComponent } from 'app/game/components/canvas/canvas.component';
 import { BaseGameWindowComponent } from '../base-game.component';
 import { FlappyBird, FlappyBirdState } from './models/flappy-bird.class';
+import { PlayerSourceType } from 'app/shared/models/player-source-type.enum';
 
 @Component({
   selector: 'app-flappy-bird',
@@ -28,6 +29,10 @@ export class FlappyBirdComponent
     this.resetGame();
   }
 
+  public override ngOnDestroy(): void {
+    super.ngOnDestroy();
+  }
+
   public override restart(): void {
     this.game.state = new FlappyBirdState();
 
@@ -36,14 +41,36 @@ export class FlappyBirdComponent
 
   protected override update(): void {
     super.update();
+
+    if (!this.isPaused) {
+      this.render();
+    }
   }
 
   protected onKeyDown(event: KeyboardEvent): void {
     const player = this.game.players[0];
+
+    if (player.playerType === PlayerSourceType.KEYBOARD) {
+      switch (event.key) {
+        case ' ':
+          event.preventDefault();
+          this.game.players[0].inputData['jump'] = 1;
+          break;
+      }
+    }
   }
 
   protected onKeyUp(event: KeyboardEvent): void {
     const player = this.game.players[0];
+
+    if (player.playerType === PlayerSourceType.KEYBOARD) {
+      switch (event.key) {
+        case ' ':
+          event.preventDefault();
+          this.game.players[0].inputData['jump'] = 0;
+          break;
+      }
+    }
   }
 
   //
@@ -59,5 +86,11 @@ export class FlappyBirdComponent
       this.game.state.score = 0;
       this.game.state.isGameActive = false;
     });
+  }
+
+  private render(): void {
+    const context = this._canvas.getContext('2d');
+    if (!context) return;
+    context.clearRect(0, 0, this._canvas.width, this._canvas.height);
   }
 }
