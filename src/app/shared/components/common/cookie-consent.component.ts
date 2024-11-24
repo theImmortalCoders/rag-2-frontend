037cfda8a1b690faadce8f56cc4984d8ab87bd4e
Paweł Buczek <pbuczek7@wp.pl>
2024-11-24 17:41:31 +0100

    feat: #174 added icon and title of modal

--- src/app/shared/components/common/cookie-consent.component.ts
+++ src/app/shared/components/common/cookie-consent.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-cookie-consent',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   template: `
     <div
       class="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50">
       <div
         class="bg-lightGray rounded-lg shadow-lg p-6 text-center border-2 border-mainOrange">
+        <div
+          class="flex flex-row space-x-2 w-full items-center justify-center pb-4">
+          <h1 class="text-2xl text-mainOrange font-semibold">COOKIE CONSENT</h1>
+          <mat-icon class="text-mainOrange text-xl font-thin">cookie</mat-icon>
+        </div>
         <p class="text-mainCreme mb-4">
           This website uses cookies to ensure the best quality of service.
           Please accept cookies to continue using the site.
