5c1f1ac85c702bf15fd10fec196c94c666f32d40
Paweł Buczek <pbuczek7@wp.pl>
2025-02-03 20:04:30 +0100

    feat: #219 prepared new mode field

--- src/app/game/components/console/console.component.ts
+++ src/app/game/components/console/console.component.ts
@@ -16,7 +16,11 @@ import { ConsoleFieldsetComponent } from './console-fieldset/console-fieldset.co
         [className]="
           consoleClasses['consoleContainer'] +
           (isConsoleVisible ? ' h-72 py-4' : ' h-0')
-        ">
+          ">
+        <div class="w-full absolute right-4 top-6 flex flex-row space-x-2 justify-end items-center font-mono text-black hover:text-mainCreme transition-all ease-in-out duration-200">
+          <span class="uppercase">External console mode</span>
+          <i data-feather="external-link" class="size-5"></i>
+        </div>
         <app-console-fieldset
           [logData]="logData | exchange_data"
           [className]="
@@ -34,7 +38,7 @@ export class ConsoleComponent {
   public consoleClasses: TExchangeData = {
     button: `w-full bg-lightGray tracking-[0.15em] sticky z-50 top-0 transition-all ease-in-out duration-700 border-b-2
       border-mainOrange hover:border-green-500 text-center py-2 uppercase font-bold font-mono text-xl cursor-pointer`,
-    consoleContainer: `w-full max-h-96 transition-all ease-in-out duration-700 bg-lightGray overflow-y-scroll z-50 px-5`,
+    consoleContainer: `relative w-full max-h-96 transition-all ease-in-out duration-700 bg-lightGray overflow-y-scroll z-50 px-5`,
     consoleFieldset: `items-start gap-y-6 transition-all ease-in-out duration-700`,
   };
 
