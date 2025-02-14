b37a1a48ae7e3ca4d5202288b4a1c5ff04b1f99e
Paweł Buczek <pbuczek7@wp.pl>
2025-02-15 00:19:17 +0100

    refactor: #224 tables html structure refactored

--- src/app/dashboard/components/shared/recorded-game-table/recorded-game-table.component.ts
+++ src/app/dashboard/components/shared/recorded-game-table/recorded-game-table.component.ts
@@ -12,14 +12,15 @@ import { LoadingSpinnerComponent } from '@commonComponents/loading-spinner.compo
       <app-loading-spinner />
     } @else {
       @if (recordedGamesData && recordedGamesData.length > 0) {
-        <div
+        <table
           class="flex flex-col min-w-[44rem] w-full justify-around space-y-0 font-mono">
-          <div
+          <tr
             class="flex flex-row space-x-4 justify-between bg-mainGray text-mainOrange text-sm xs:text-base font-bold px-4 py-2">
-            <span class="flex justify-center w-[5%]">No.</span>
-            <span class="flex justify-center w-2/12">Game name</span>
-            <span class="flex justify-center w-3/12">Game start date</span>
-            <div
+            <th id="no" class="flex justify-center w-[5%]">No.</th>
+            <th id="game_name" class="flex justify-center w-2/12">Game name</th>
+            <th id="game_start_date" class="flex justify-center w-3/12">Game start date</th>
+            <th
+              id="game_end_date"
               class="flex flex-row gap-x-1 items-center justify-center w-3/12">
               <button (click)="setSortingBy('Ended')">Game end date</button>
               @if (sortBy === 'Ended') {
@@ -34,8 +35,9 @@ import { LoadingSpinnerComponent } from '@commonComponents/loading-spinner.compo
                   <i data-feather="minus" class="size-4 "></i>
                 </span>
               }
-            </div>
-            <button
+            </th>
+            <th
+              id="size"
               class="flex flex-row gap-x-1 items-center justify-center w-1/12">
               <button (click)="setSortingBy('SizeMb')">Size</button>
               @if (sortBy === 'SizeMb') {
@@ -50,53 +52,57 @@ import { LoadingSpinnerComponent } from '@commonComponents/loading-spinner.compo
                   <i data-feather="minus" class="size-4 "></i>
                 </span>
               }
-            </button>
-            <span class="flex justify-center w-1/12">Download</span>
-            <span class="flex justify-center w-1/12">Delete</span>
-          </div>
+            </th>
+            <th id="download" class="flex justify-center w-1/12">Download</th>
+            <th id="delete" class="flex justify-center w-1/12">Delete</th>
+          </tr>
           @for (recordedGame of recordedGamesData; track recordedGame.id) {
-            <div
+            <tr
               class="flex flex-row space-x-4 justify-between px-4 py-2 text-mainCreme text-sm xs:text-base opacity-80 hover:opacity-100 {{
                 $even ? 'bg-lightGray' : 'bg-darkGray'
               }}">
-              <span class="flex justify-center w-[5%]">{{ $index + 1 }}.</span>
-              <span class="flex justify-center w-2/12 uppercase">{{
+              <td headers="no" class="flex justify-center w-[5%]">{{ $index + 1 }}.</td>
+              <td headers="game_name" class="flex justify-center w-2/12 uppercase">{{
                 recordedGame.gameName
-              }}</span>
-              <span class="flex justify-center w-3/12">{{
+              }}</td>
+              <td headers="game_start_date" class="flex justify-center w-3/12">{{
                 recordedGame.started | date: 'dd/MM/yyyy, HH:mm:ss'
-              }}</span>
-              <span class="flex justify-center w-3/12">{{
+              }}</td>
+              <td headers="game_end_date" class="flex justify-center w-3/12">{{
                 recordedGame.ended | date: 'dd/MM/yyyy, HH:mm:ss'
-              }}</span>
-              <span class="flex justify-center w-1/12 text-nowrap">{{
+              }}</td>
+              <td headers="size" class="flex justify-center w-1/12 text-nowrap">{{
                 recordedGame.isEmptyRecord
                   ? '-'
                   : recordedGame.sizeMb.toPrecision(2) + ' MB'
-              }}</span>
-              @if (recordedGame.isEmptyRecord) {
-                <span class="flex group justify-center w-1/12"> </span>
-              } @else {
+              }}</td>
+              <td headers="download" class="flex justify-center w-1/12">
+                @if (recordedGame.isEmptyRecord) {
+                  <span> </span>
+                } @else {
+                  <button
+                    id="downloadButton"
+                    class="group"
+                    (click)="downloadEmitter.emit(recordedGame.id)">
+                    <i
+                      data-feather="download"
+                      class="text-mainCreme group-hover:text-green-500 size-4 xs:size-5"></i>
+                  </button>
+                }
+              </td>
+              <td headers="delete" class="flex justify-center w-1/12">
                 <button
-                  id="downloadButton"
-                  class="flex group justify-center w-1/12"
-                  (click)="downloadEmitter.emit(recordedGame.id)">
+                  id="deleteButton"
+                  class="group"
+                  (click)="deleteEmitter.emit(recordedGame.id)">
                   <i
-                    data-feather="download"
-                    class="text-mainCreme group-hover:text-green-500 size-4 xs:size-5"></i>
+                    data-feather="x-square"
+                    class="text-mainCreme group-hover:text-red-500 size-4 xs:size-5"></i>
                 </button>
-              }
-              <button
-                id="deleteButton"
-                class="flex group justify-center w-1/12"
-                (click)="deleteEmitter.emit(recordedGame.id)">
-                <i
-                  data-feather="x-square"
-                  class="text-mainCreme group-hover:text-red-500 size-4 xs:size-5"></i>
-              </button>
-            </div>
+              </td>
+            </tr>
           }
-        </div>
+        </table>
       }
     }
   `,
