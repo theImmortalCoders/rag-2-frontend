b9930670abf3ce54e77fa96735ebaf17c834aaee
Paweł Buczek <pbuczek7@wp.pl>
2025-02-15 13:54:21 +0100

    fix: #dev rwd fix

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -91,7 +91,7 @@ import { RouterModule } from '@angular/router';
         </div>
       </div>
       <div
-        class="flex flex-row space-x-6 xs:space-x-12 w-full items-center justify-center text-xs lg:text-sm text-mainOrange grayscale">
+        class="flex flex-col xs:flex-row space-y-3 xs:space-y-0 space-x-0 xs:space-x-12 w-full items-center justify-center text-sm xs:text-xs lg:text-sm text-mainOrange grayscale">
         <span>&copy; {{ currentYear }} RUT & GEST</span>
         <span>PRIVACY POLICY</span>
         <button [routerLink]="['accessibility-statement']">
