37cbbc70f2d8787efb9eb9360bae86fc2dc5750e
Paweł Buczek <pbuczek7@wp.pl>
2025-02-19 21:30:26 +0100

    feat: #229 added initial docs page

---
+++ src/app/documentation/documentation.page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documentation-page',
+  standalone: true,
+  imports: [],
+  template: ` <p>documentation works!</p> `,
+})
+export class DocumentationPageComponent {}
