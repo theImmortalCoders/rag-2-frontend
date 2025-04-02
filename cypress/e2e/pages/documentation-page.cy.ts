58b05acdba8cec0c88581a7a508d77ae0179d670
Paweł Buczek <pbuczek7@wp.pl>
2025-04-02 18:34:36 +0200

    feat: #187 added documentation page e2e tests

---
+++ cypress/e2e/pages/documentation-page.cy.ts
@@ -0,0 +1,18 @@
+describe('Home Page E2E Tests:', () => {
+  beforeEach(() => {
+    cy.visit('/documentation');
+  });
+
+  it('should describe landing page title correctly', () => {
+    cy.get('#documentationPageHeader').should(
+      'contain.text',
+      'Check the documentation we prepared to help:'
+    );
+  });
+
+  it('should describe at least 3 documentation tiles', () => {
+    cy.get('#documentationTilesContainer')
+      .children('app-single-doc')
+      .should('have.length.at.least', 3);
+  });
+});
